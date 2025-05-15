2fe72f6d50f56f93929a6d53640c74cf5fc41ad1
Disce-et-facere <krantzr@hotmail.se>
2025-05-15 20:00:09 +0200

    removed loadingscreen and cleaned up main.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import LoadingScreen from '@/views/LoadingScreen.vue';
-import Dashboard from '@/views/AppDashboard.vue';
+import { createApp } from 'vue';
+import AppDashboard from '@/views/AppDashboard.vue';
 
-const routes = [
-  {
-    path: '/',
-    name: 'Loading',
-    component: LoadingScreen,
-  },
-  {
-    path: '/dashboard',
-    name: 'AppDashboard',
-    component: Dashboard,
-  },
-];
-
-const router = createRouter({
-  history: createWebHistory(),
-  routes,
-});
-
-export default router;
+createApp(AppDashboard).mount('#app');
